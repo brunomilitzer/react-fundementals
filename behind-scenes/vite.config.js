0efa57387d4a37002170c8e09cf2f806ed37ff80
Bruno Militzer <brunomilitzer.work@gmail.com>
2024-10-10 21:18:16 +0100

    Added Section 13

---
+++ behind-scenes/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as million from "million/compiler";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [million.vite({auto: true}), react()],
+})
