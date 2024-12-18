c1bdfaf5607f5e7f64976cd1c18192e419e9f5ad
Bruno Militzer <brunomilitzer.work@gmail.com>
2024-12-18 21:57:39 +0000

    Finished Share Meals Section

---
+++ foodies/lib/actions.js
@@ -0,0 +1,36 @@
+'use server';
+
+import {saveMeal} from "@/lib/meals";
+import {redirect} from "next/navigation";
+
+function isInvalidTest(text) {
+    return !text || text.trim() === ''
+}
+
+export async function shareMeal(prevState, formData) {
+    const meal = {
+        title: formData.get('title'),
+        summary: formData.get('summary'),
+        instructions: formData.get('instructions'),
+        image: formData.get('image'),
+        creator: formData.get('name'),
+        creator_email: formData.get('email')
+    };
+
+    if (isInvalidTest(meal.title)
+        || isInvalidTest(meal.summary)
+        || isInvalidTest(meal.instructions)
+        || isInvalidTest(meal.creator)
+        || isInvalidTest(meal.creator_email)
+        || !meal.creator_email.includes('@')
+        || !meal.image
+        || meal.image.size === 0) {
+
+        return {
+            message: "invalid input."
+        };
+    }
+
+    await saveMeal(meal);
+    redirect('/meals');
+}
