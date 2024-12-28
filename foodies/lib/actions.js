deb1d0b646297270432055dad46256cddd23a333
Bruno Militzer <brunomilitzer.work@gmail.com>
2024-12-28 21:05:38 +0000

    Finished dynamic metadata

--- foodies/lib/actions.js
+++ foodies/lib/actions.js
@@ -2,6 +2,7 @@
 
 import {saveMeal} from "@/lib/meals";
 import {redirect} from "next/navigation";
+import {revalidatePath} from "next/cache";
 
 function isInvalidTest(text) {
     return !text || text.trim() === ''
@@ -32,5 +33,8 @@ export async function shareMeal(prevState, formData) {
     }
 
     await saveMeal(meal);
+
+    revalidatePath('/meals');
+
     redirect('/meals');
 }
