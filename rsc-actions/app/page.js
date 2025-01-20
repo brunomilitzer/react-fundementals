98541e5fde95a70e2010a4127a35368bc98cb2d0
Bruno Militzer <brunomilitzer.work@gmail.com>
2025-01-20 20:04:59 +0000

    Added RSC Components

---
+++ rsc-actions/app/page.js
@@ -0,0 +1,28 @@
+import {Suspense} from 'react';
+import UsePromiseDemo from "@/components/UsePromiseDemo";
+import fs from "node:fs/promises";
+import ErrorBoundary from "@/components/ErrorBoundry";
+
+export default async function Home() {
+    const fetchUsersPromise = new Promise((resolve, reject) => setTimeout(async () => {
+        const data = await fs.readFile('dummy-db.json', 'utf-8');
+        const users = JSON.parse(data);
+        //resolve(users);
+        reject(new Error('Error!'));
+    }, 2000));
+
+    return (
+        <main>
+            {/*<ClientDemo>
+                <RSCDemo/>
+            </ClientDemo>*/}
+            {/*<DataFetchingDemo />*/}
+            {/*<ServerActionsDemo />*/}
+            <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+                <Suspense fallback={<p>Loading users...</p>}>
+                    <UsePromiseDemo usersPromise={fetchUsersPromise}/>
+                </Suspense>
+            </ErrorBoundary>
+        </main>
+    );
+}
